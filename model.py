b0aa47d06ad40f3d33d358e3bcfc0558901e5e9f
Nilu Isakova <niluisakova@gmail.com>
2023-09-21 08:48:12 -0500

    added winner check function

--- model.py
+++ model.py
@@ -8,3 +8,16 @@ class TicTacToeModel:
             self.matrix3x3[row][column] = value
             return True
         return False
+
+    def winner_check(self, value):
+        for i in range(3):
+            if (self.matrix3x3[i][0] == self.matrix3x3[i][1] == self.matrix3x3[i][2] == value) or \
+               (self.matrix3x3[0][i] == self.matrix3x3[1][i] == self.matrix3x3[2][i] == value):
+                return True
+
+        # check diagonals
+        if (self.matrix3x3[0][0] == self.matrix3x3[1][1] == self.matrix3x3[2][2] == value) or \
+           (self.matrix3x3[0][2] == self.matrix3x3[1][1] == self.matrix3x3[2][0] == value):
+            return True
+
+        return False
